cb39b0a21f793a3692a6b0667ba14d18188c10d6
Koyamin <koyafumin@gmail.com>
2026-03-22 23:11:41 +0800

    test: 初始化测试文件

---
+++ build.lua
@@ -0,0 +1,12 @@
+module = "ecnuthesis"
+
+checkengines = {"xetex"}
+stdengine = "xetex"
+checkopts = "-interaction=batchmode"
+installfiles = {"*.cls", "*.sty", "*.def"}
+
+typesetfiles = {"ecnuthesis.dtx"}
+typesetexe = "xelatex"
+
+unpackfiles = {"ecnuthesis.ins"}
+unpackexe = "xetex"
